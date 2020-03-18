30e4aa15f2c3175d10211154066fa5ae76f2e4a8
Marc-André Moreau <mamoreau@devolutions.net>
2020-03-18 10:56:59 -0400

    WaykDen-ps: update docker image tags

--- WaykDen/Public/WaykDenService.ps1
+++ WaykDen/Public/WaykDenService.ps1
@@ -13,7 +13,7 @@ function Get-WaykDenImage
         [ordered]@{ # Linux containers
             "den-lucid" = "devolutions/den-lucid:3.6.5-buster";
             "den-picky" = "devolutions/picky:4.2.1-buster";
-            "den-server" = "devolutions/den-server:1.14.0-buster-dev";
+            "den-server" = "devolutions/den-server:1.14.0-buster";
 
             "den-mongo" = "library/mongo:4.2-bionic";
             "den-traefik" = "library/traefik:1.7";
@@ -24,7 +24,7 @@ function Get-WaykDenImage
         [ordered]@{ # Windows containers
             "den-lucid" = "devolutions/den-lucid:3.6.5-servercore-ltsc2019";
             "den-picky" = "devolutions/picky:4.2.1-servercore-ltsc2019";
-            "den-server" = "devolutions/den-server:1.14.0-servercore-ltsc2019-dev";
+            "den-server" = "devolutions/den-server:1.14.0-servercore-ltsc2019";
 
             "den-mongo" = "library/mongo:4.2-windowsservercore-1809";
             "den-traefik" = "library/traefik:1.7-windowsservercore-1809";
